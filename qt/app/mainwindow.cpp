0b09806927647f1b418ab14e8c46313c9d85af04
noahvaks <noahvaks@gmail.com>
2020-09-09 22:00:07 -0700

    enums
    added enums and page switching the view cities and view food pages

--- qt/app/mainwindow.cpp
+++ qt/app/mainwindow.cpp
@@ -6,6 +6,8 @@ MainWindow::MainWindow(QWidget *parent)
 	, ui(new Ui::MainWindow)
 {
 	ui->setupUi(this);
+
+    ui->stackedWidget_pages->setCurrentIndex(HOME); // setCurrentIndex cycles through the stackedwidget
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +15,41 @@ MainWindow::~MainWindow()
 	delete ui;
 }
 
+/*----PAGE NAVIGATION----*/
+/*----HOME----*/
+void MainWindow::on_pushButton_home_cities_clicked()
+{
+    ui->stackedWidget_pages->setCurrentIndex(CITIES);
+}
+
+void MainWindow::on_pushButton_home_food_clicked()
+{
+    ui->stackedWidget_pages->setCurrentIndex(FOOD);
+}
+
+void MainWindow::on_pushButton_home_berlin_clicked()
+{
+    ui->stackedWidget_pages->setCurrentIndex(BERLIN);
+}
+
+void MainWindow::on_pushButton_home_paris_clicked()
+{
+    ui->stackedWidget_pages->setCurrentIndex(PARIS);
+}
+
+void MainWindow::on_pushButton_home_custom_clicked()
+{
+    ui->stackedWidget_pages->setCurrentIndex(CUSTOM);
+}
+
+/*----CITIES----*/
+void MainWindow::on_pushButton_cities_back_clicked()
+{
+    ui->stackedWidget_pages->setCurrentIndex(HOME);
+}
+
+/*----FOOD----*/
+void MainWindow::on_pushButton_food_back_clicked()
+{
+    ui->stackedWidget_pages->setCurrentIndex(HOME);
+}
--- qt/app/mainwindow.h
+++ qt/app/mainwindow.h
@@ -15,6 +15,40 @@ public:
 	MainWindow(QWidget *parent = nullptr);
 	~MainWindow();
 
+    void clearFields() // function to reset all tables and user inputs
+    {
+
+    }
+
+    enum page // enums for navigation through stackedwidget
+    {
+        HOME,
+        CITIES,
+        FOOD,
+        PARIS,
+        CUSTOM,
+        PURCHASE,
+        RECEIPT,
+        LOGIN,
+        ADMIN
+    };
+
+
+private slots:
+    void on_pushButton_home_cities_clicked();
+
+    void on_pushButton_home_food_clicked();
+
+    void on_pushButton_home_berlin_clicked();
+
+    void on_pushButton_home_paris_clicked();
+
+    void on_pushButton_home_custom_clicked();
+
+    void on_pushButton_cities_back_clicked();
+
+    void on_pushButton_food_back_clicked();
+
 private:
 	Ui::MainWindow *ui;
 };
